9711d2a2dfb560fa9066e5fdca833b8814506394
Arthur <arthur.queiroz08@aluno.ifce.edu.br>
2024-05-22 20:11:23 -0300

    add documentation (swagger)

--- api/controller/category_controller.go
+++ api/controller/category_controller.go
@@ -16,77 +16,135 @@ func NewCategoryController(cs domain.CategoryService) *CategoryController {
 	}
 }
 
+// Create Category
+//
+// @Summary Create a new category
+// @Description Create a new category with the provided details
+// @Tags Categories
+// @Accept json
+// @Produce json
+// @Param category body dto.CategoryDTO true "Category DTO"
+// @Success 200 {object} dto.CategoryDTO
+// @Failure 400 {object} map[string]interface{}
+// @Failure 500 {object} map[string]interface{}
+// @Router /categories [post]
 func (cc *CategoryController) Create(c *fiber.Ctx) error {
 	var categoryDTO dto.CategoryDTO
 
 	if err := c.BodyParser(&categoryDTO); err != nil {
-		return err
+		return c.Status(fiber.StatusBadRequest).JSON(map[string]interface{}{"message": "Invalid request body"})
 	}
 
 	category := categoryDTO.ParseToEntity()
 
 	err := cc.cs.Create(category)
 	if err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(map[string]interface{}{"message": "Could not create category"})
 	}
 
 	return c.JSON(dto.FromEntity(*category))
 }
 
+// GetCategory
+//
+// @Summary Get a category by ID
+// @Description Retrieve a category by its ID
+// @Tags Categories
+// @Produce json
+// @Param id path int true "Category ID"
+// @Success 200 {object} dto.CategoryDTO
+// @Failure 400 {object} map[string]interface{}
+// @Failure 404 {object} map[string]interface{}
+// @Router /categories/{id} [get]
 func (cc *CategoryController) GetCategory(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 	if err != nil {
 		c.Status(fiber.StatusBadRequest)
-		return c.JSON(fiber.Map{"message": "id is required"})
+		return c.JSON(map[string]interface{}{"message": "ID is required"})
 	}
 	category, err := cc.cs.GetById(id)
 	if err != nil {
 		c.Status(fiber.StatusNotFound)
-		return c.JSON(fiber.Map{"message": "category not found"})
+		return c.JSON(map[string]interface{}{"message": "Category not found"})
 	}
 
 	c.Status(fiber.StatusOK)
 	return c.JSON(dto.FromEntity(*category))
 }
 
+// DeleteCategory
+//
+// @Summary Delete a category by ID
+// @Description Delete a category by its ID
+// @Tags Categories
+// @Param id path int true "Category ID"
+// @Success 204 "No Content"
+// @Failure 400 {object} map[string]interface{}
+// @Failure 404 {object} map[string]interface{}
+// @Router /categories/{id} [delete]
 func (cc *CategoryController) DeleteCategory(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 	if err != nil {
 		c.Status(fiber.StatusBadRequest)
-		return c.JSON(fiber.Map{"message": "id is required"})
+		return c.JSON(map[string]interface{}{"message": "ID is required"})
 	}
 
 	err = cc.cs.Delete(id)
 	if err != nil {
 		c.Status(fiber.StatusNotFound)
-		return c.JSON(fiber.Map{"message": "category not found"})
+		return c.JSON(map[string]interface{}{"message": "Category not found"})
 	}
 
 	c.Status(fiber.StatusNoContent)
 	return nil
 }
 
+// UpdateCategory
+//
+// @Summary Update a category by ID
+// @Description Update a category with the provided details
+// @Tags Categories
+// @Accept json
+// @Produce json
+// @Param id path int true "Category ID"
+// @Param category body dto.CategoryDTO true "Category DTO"
+// @Success 204 "No Content"
+// @Failure 400 {object} map[string]interface{}
+// @Failure 404 {object} map[string]interface{}
+// @Router /categories/{id} [put]
 func (cc *CategoryController) UpdateCategory(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 	if err != nil {
 		c.Status(fiber.StatusBadRequest)
-		return c.JSON(fiber.Map{"message": "id is required"})
+		return c.JSON(map[string]interface{}{"message": "ID is required"})
 	}
 
 	var categoryDTO dto.CategoryDTO
 	if err := c.BodyParser(&categoryDTO); err != nil {
-		return err
+		return c.Status(fiber.StatusBadRequest).JSON(map[string]interface{}{"message": "Invalid request body"})
 	}
 	err = cc.cs.Update(id, categoryDTO.ParseToEntity())
 	if err != nil {
 		c.Status(fiber.StatusNotFound)
-		return c.JSON(fiber.Map{"message": "category not found"})
+		return c.JSON(map[string]interface{}{"message": "Category not found"})
 	}
 
 	c.Status(fiber.StatusNoContent)
 	return nil
 }
 
+// GetAllCategory
+//
+// @Summary Get all categories with pagination and optional filtering by name
+// @Description Retrieve all categories, with pagination and optional filtering by name
+// @Tags Categories
+// @Produce json
+// @Param page query int false "Page number" default(0)
+// @Param size query int false "Page size" default(10)
+// @Param name query string false "Category name"
+// @Success 200 {array} dto.CategoryDTO
+// @Failure 500 {object} map[string]interface{}
+// @Router /categories [get]
 func (cc *CategoryController) GetAllCategory(c *fiber.Ctx) error {
 	page := c.QueryInt("page", 0)
 	size := c.QueryInt("size", 10)
@@ -95,7 +153,7 @@ func (cc *CategoryController) GetAllCategory(c *fiber.Ctx) error {
 	all, err := cc.cs.GetAll(page, size, name)
 	if err != nil {
 		c.Status(fiber.StatusInternalServerError)
-		return c.JSON(fiber.Map{"message": "internal server error"})
+		return c.JSON(map[string]interface{}{"message": "Internal server error"})
 	}
 
 	c.Status(fiber.StatusOK)
--- api/route/route.go
+++ api/route/route.go
@@ -1,12 +1,25 @@
 package route
 
 import (
+	_ "crud-golang/docs"
+	swagger "github.com/arsmn/fiber-swagger/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	"gorm.io/gorm"
 )
 
+// @title Category CRUD
+// @version 1.0
+// @description This is a sample CRUD.
+// @contact.name API Support
+// @contact.url http://suport.com
+// @contact.email support@dev.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8000
+// @BasePath /
+// @schemes http
 func Setup(db gorm.DB, app *fiber.App) {
 
 	app.Use(recover.New())
@@ -17,6 +30,7 @@ func Setup(db gorm.DB, app *fiber.App) {
 	}))
 
 	publicRouter := app.Group("")
+	publicRouter.Get("/swagger/*", swagger.HandlerDefault)
 	//privateRouter := app.Group("")
 
 	NewCategoryRouter(db, publicRouter)
